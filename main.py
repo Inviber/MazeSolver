6e117fd666bf7d34199e30328273306127ae1b75
Inviber <bkelley4600@gmail.com>
2023-10-16 20:41:23 -0500

    Added line class and functionality for drawing

--- main.py
+++ main.py
@@ -6,13 +6,16 @@ class Window:
         self.height = height
         self.root = Tk()
         self.root.title = "Maze Solver"
-        self.canvas = Canvas()
+        self.canvas = Canvas(self.root, bg="white", height=height, width=width)
         self.canvas.pack()
         self.running = False
     
     def redraw(self):
         self.root.update_idletasks()
         self.root.update()
+
+    def draw_line(self, line, fill_color="black"):
+        line.draw(self.canvas, fill_color)
     
     def wait_for_close(self):
         self.running = True
@@ -23,8 +26,27 @@ class Window:
         self.running = False
         self.root.protocol("WM_DELETE_WINDOW", self.close)
 
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+class Line:
+    def __init__(self, p1, p2):
+        self.p1 = p1
+        self.p2 = p2
+    
+    def draw(self, canvas, fill_color):
+        canvas.create_line(self.p1.x, self.p1.y, self.p2.x, self.p2.y, fill=fill_color, width=2)
+        canvas.pack()
+
 def main():
     win = Window(800,600)
+    p1 = Point(100,100)
+    p2 = Point(300,300)
+    line = Line(p1,p2)
+    win.draw_line(line, "black")
     win.wait_for_close()
+    win.close()
 
 main()
