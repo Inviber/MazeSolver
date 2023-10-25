4d043ca3b10485586d0284163cb5f34c6007a3f7
Inviber <bkelley4600@gmail.com>
2023-10-24 20:31:14 -0500

    Added breaking down walls to create the maze

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from tkinter import Tk, BOTH, Canvas
 import time
+import random
 
 class Window:
     def __init__(self, width, height):
@@ -127,7 +128,7 @@ class Cell:
             self._win.draw_line(line, fill_color)  
 
 class Maze:
-    def __init__(self, x1, y1, num_rows, num_cols, cell_size_x, cell_size_y, win=None):
+    def __init__(self, x1, y1, num_rows, num_cols, cell_size_x, cell_size_y, win=None, seed=None):
         self._cells = []
         self._x1 = x1
         self._y1 = y1
@@ -136,8 +137,11 @@ class Maze:
         self._cell_size_x = cell_size_x
         self._cell_size_y = cell_size_y
         self._win = win
+        if seed:
+            random.seed(seed)
         self._create_cells()
         self._break_entrance_and_exit()
+        self._break_walls_r(0,0)
     
     def _create_cells(self):
         for i in range(self._num_cols):
@@ -170,6 +174,51 @@ class Maze:
         self._draw_cell(0,0)
         self._cells[self._num_cols-1][self._num_rows-1].has_bottom_wall = False      #Break for exit
         self._draw_cell(self._num_cols-1, self._num_rows-1)
+    
+    def _break_walls_r(self, i, j):
+        self._cells[i][j].visited = True
+        while True:
+            possibleCells = []
+
+            # top
+            if j > 0 and not self._cells[i][j-1].visited:
+                possibleCells.append((i,j-1))
+            # right
+            if i < self._num_cols-1 and not self._cells[i+1][j].visited:
+                possibleCells.append((i+1,j))
+            # bottom
+            if j < self._num_rows-1 and not self._cells[i][j+1].visited:
+                possibleCells.append((i,j+1))
+            # left
+            if i > 0 and not self._cells[i-1][j].visited:
+                possibleCells.append((i-1,j))
+            
+            if len(possibleCells) == 0:
+                self._draw_cell(i,j)
+                return
+            
+            direction = random.randrange(len(possibleCells))
+            possible = possibleCells[direction]
+            
+            # knock walls
+            # top
+            if possible[1] == j - 1:
+                self._cells[i][j].has_top_wall = False
+                self._cells[i][j-1].has_bottom_wall = False
+            # right
+            if possible[0] == i + 1:
+                self._cells[i][j].has_right_wall = False
+                self._cells[i+1][j].has_left_wall = False
+            # bottom
+            if possible[1] == j + 1:
+                self._cells[i][j].has_bottom_wall = False
+                self._cells[i][j+1].has_top_wall = False
+            # left
+            if possible[0] == i - 1:
+                self._cells[i][j].has_left_wall = False
+                self._cells[i-1][j].has_right_wall = False
+
+            self._break_walls_r(possible[0], possible[1])
 
 def main():
     num_rows = 12
@@ -181,7 +230,7 @@ def main():
     cell_size_y = (screen_y - 2 * margin) / num_rows
     win = Window(screen_x, screen_y)
 
-    maze = Maze(margin, margin, num_rows, num_cols, cell_size_x, cell_size_y, win)
+    maze = Maze(margin, margin, num_rows, num_cols, cell_size_x, cell_size_y, win, 10)
 
     win.wait_for_close()
 
